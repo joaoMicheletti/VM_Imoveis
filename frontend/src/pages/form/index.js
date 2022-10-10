258f38769f89c59b1b84fa39a96f65dacd93bc14
João Victor <jv604014@gmail.com>
2022-10-10 17:59:34 -0300

    update on index.js
    implementação no formulário, criando as constantes com o useState  para receberem os valores do formulário.

--- frontend/src/pages/form/index.js
+++ frontend/src/pages/form/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import {React, useState} from "react";
 import { Link } from "react-router-dom";
 import './style.css';
  
@@ -6,6 +6,25 @@ import {AiFillFacebook} from 'react-icons/ai';
 import {RiInstagramFill} from 'react-icons/ri';
 
 export default function Loja(){
+    const [Rua, SetRua] = useState('');
+    const [Numero, SetNumero] = useState('');
+    const [Cep, SetCep] = useState('');
+    const [Vendedor, SetVendedor] = useState('');
+    const [Contato, SetContato] = useState('');
+    const [Description, SetDescription] = useState('');
+    const [Image, SetImage] = useState('');
+    
+    const Data = {
+        Rua,
+        Numero,
+        Cep,
+        Vendedor,
+        Contato,
+        Description,
+        Image
+    } 
+    console.log(Data);
+
     return(
         <div id="Container_Loja">
 
@@ -21,14 +40,49 @@ export default function Loja(){
             <section id="Conteudo_Form">
                 
                 <form id="Formulario">
-                    <p id="paragrafo">Rua:</p><input id="entrada" type="text" placeholder="Endereço:"/>
-                    <p id="paragrafo">Número:</p><input id="entrada" type="text" placeholder="Numero da Casa:"/>
-                    <p id="paragrafo">CEP:</p><input id="entrada" type="number" placeholder="CEP:"/>
-                    <p id="paragrafo">Vendedor:</p><input id="entrada" type="text" placeholder="Nome do Vendenor"/>
-                    <p id="paragrafo">Contato:</p><input id="entrada" type="number" placeholder="EX: 11932223533"/>
-                    <p id="paragrafo">Descrição:</p><input id="entrada" type="text" placeholder="EX: 100m³ 3quartos 2 banheiros"/>
-                    <p id="paragrafo">Carregar Imagem:</p><input id="entrada" type='file' />
-                    <input id="send" type="submit" value='send'></input>
+                    <p id="paragrafo">Rua:</p>
+                    <input id="entrada"
+                     type="text" 
+                     placeholder="Endereço:"
+                     onChange={(e) => SetRua(e.target.value)}/>
+
+                    <p id="paragrafo">Número:</p>
+                    <input id="entrada" 
+                    type="text" 
+                    placeholder="Numero da Casa:"
+                    onChange={(e) => SetNumero(e.target.value)}/>
+
+                    <p id="paragrafo">CEP:</p>
+                    <input id="entrada" 
+                    type="number" 
+                    placeholder="CEP:"
+                    onChange={(e) => SetCep(e.target.value)}/>
+
+                    <p id="paragrafo">Vendedor:</p>
+                    <input id="entrada" 
+                    type="text" 
+                    placeholder="Nome do Vendenor"
+                    onChange={(e) => SetVendedor(e.target.value)}/>
+
+                    <p id="paragrafo">Contato:</p>
+                    <input id="entrada" 
+                    type="number" 
+                    placeholder="EX: 11932223533"
+                    onCanPlay={(e) => SetContato(e.target.value)}/>
+
+                    <p id="paragrafo">Descrição:</p>
+                    <input id="entrada" 
+                    type="text" 
+                    placeholder="EX: 100m³ 3quartos 2 banheiros"
+                    onChange={(e) => SetDescription(e.target.value)}/>
+                    
+                    <p id="paragrafo">Carregar Imagem:</p>
+                    <input id="entrada" 
+                    type='file' 
+                    onChange={(e) => SetImage(e.target.value)}/>
+                    <input id="send" 
+                    type="submit" 
+                    value='send'></input>
 
                 </form>
 
